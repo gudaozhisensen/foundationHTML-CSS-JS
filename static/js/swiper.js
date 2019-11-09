bd7a4fd9c94f4f76e7b15dddce4892e1a5d75a8a
gudaozhisensen <593866511@qq.com>
2019-11-09 19:35:29 +0800

    almostdone

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -9,6 +9,51 @@ function getStyle(obj, name)
                 return getComputedStyle(obj, null)[name];
             }
         }
+function prevNextMove(obj, json, fnEnd) {
+    clearInterval(obj.timer);
+    obj.timer=function (){
+        var bStop=true;		//假设：所有值都已经到了
+        
+        for(var attr in json)
+        
+        // console.lo(attr);
+        {
+            var cur=0;
+            
+            if(attr=='opacity')
+            {
+                cur=Math.round(parseFloat(getStyle(obj, attr))*100);
+            }
+            else
+            {
+                cur=parseInt(getStyle(obj, attr));
+            }
+            
+            var speed=(json[attr]-cur)/6;
+            speed=speed>0?Math.ceil(speed):Math.floor(speed);
+            
+            if(cur!=json[attr])
+                bStop=false;
+            
+            if(attr=='opacity')
+            {
+                obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+                obj.style.opacity=(cur+speed)/100;
+            }
+            else
+            {
+                obj.style[attr]=cur+speed+'px';
+            }
+        }
+        
+        if(bStop)
+        {
+            clearInterval(obj.timer);
+                        
+            if(fnEnd)fnEnd();
+        }
+    };
+}
 
     function startMove(obj, json, fnEnd)
         {
